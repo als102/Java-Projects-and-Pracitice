a71309809bf78234e338cda6023ff45e4bf56fcc
Austin Storms <austins@scribd.com>
2022-03-27 15:39:34 -0700

    Coding Exercise 14

--- ForStatement/src/learnprogramming/Main.java
+++ ForStatement/src/learnprogramming/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-       // System.out.println("10,000 at 2% interest= " + calculateInterest(10000.00, 2.0) + " dollars");
+        // System.out.println("10,000 at 2% interest= " + calculateInterest(10000.00, 2.0) + " dollars");
         /***
          * For(init= initialize variable; termination= variable executed until i is false; increment= end of loop increase the value of i)
          String.format() to format  to float */
@@ -26,6 +26,25 @@ public class Main {
                 }
             }
         }
+
+        int sum = 0;
+        int timesCount = 0;
+
+        for (int i = 1; i <= 1000; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                timesCount++;
+                sum += i;
+                System.out.println("Found number = " + i);
+            }
+            if (timesCount == 5) {
+                break;
+            }
+
+        }
+        System.out.println("Sum = " + sum);
+
+        System.out.println(SumOddRange.isOdd(22));
+        System.out.println(SumOddRange.sumOdd(100,100));
     }
 
     public static double calculateInterest(double amount, double interestRate) {
@@ -39,7 +58,7 @@ public class Main {
             return false;
         }
         for (int i = 2; i <= (long) Math.sqrt(n); i++) {
-            System.out.println("Looping " + i);
+            // System.out.println("Looping " + i);
             if (n % i == 0) {
                 return false;
             }
---
+++ ForStatement/src/learnprogramming/SumOddRange.java
@@ -0,0 +1,22 @@
+package learnprogramming;
+
+public class SumOddRange {
+    public static boolean isOdd(int number) {
+        if (number % 2 > 0) {
+            return true;
+        } else return false;
+    }
+
+    public static int sumOdd(int start, int end) {
+        int sum = 0;
+        if (start > 0 && end > 0 && end >= start) {
+            for (int i = start; i <= end; i++) {
+                if (isOdd(i)) {
+                    sum += i;
+                }
+            }
+            return sum;
+        } else
+            return -1;
+    }
+}
