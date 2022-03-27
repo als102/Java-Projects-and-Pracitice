27967ed078a71e6a021cb8b39a19b3348e3ad461
Austin Storms <austins@scribd.com>
2022-03-27 14:02:44 -0700

    for statements

--- ForStatement/src/learnprogramming/Main.java
+++ ForStatement/src/learnprogramming/Main.java
@@ -8,15 +8,24 @@ public class Main {
         /***
          * For(init= initialize variable; termination= variable executed until i is false; increment= end of loop increase the value of i)
          String.format() to format  to float */
-        for (int i = 2; i < 9; i++) {
-            System.out.println( "10,000 at " +i+ "% interest = " + String.format("%.2f",calculateInterest(10000.00, i)));
+//        for (int i = 2; i < 9; i++) {
+//            System.out.println( "10,000 at " +i+ "% interest = " + String.format("%.2f",calculateInterest(10000.00, i)));
+//        }
+//        System.out.println("=================");
+//        for (int i = 8; i > 1; i--) {
+//            System.out.println( "10,000 at " +i+ "% interest = " + String.format("%.2f",calculateInterest(10000.00, i)));
+//        }
+        int count = 0;
+        for (int i = 10; i < 50; i++) {
+            if (isPrime(i)) {
+                count++;
+                System.out.println("Number " + i + " is a prime number");
+                if (count == 10) {
+                    System.out.println("Exit the loop");
+                    break;
+                }
+            }
         }
-        System.out.println("=================");
-        for (int i = 8; i > 1; i--) {
-            System.out.println( "10,000 at " +i+ "% interest = " + String.format("%.2f",calculateInterest(10000.00, i)));
-        }
-
-
     }
 
     public static double calculateInterest(double amount, double interestRate) {
@@ -25,15 +34,17 @@ public class Main {
     }
 
     //Prime number function evenly or by itself. Whole number greater than 1
-    public static boolean isPrime(int n){
-        if( n == 0){
+    public static boolean isPrime(int n) {
+        if (n == 0) {
             return false;
         }
-        for (int i = 2; i<= n/2; i++){
-            if(n % i == 0){
+        for (int i = 2; i <= (long) Math.sqrt(n); i++) {
+            System.out.println("Looping " + i);
+            if (n % i == 0) {
                 return false;
             }
-        } return true;
+        }
+        return true;
     }
 
 }
