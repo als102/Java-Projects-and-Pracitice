68e2002ea0d7d2e2a2cd00bb48f859dbd06f64cd
Austin Storms <austins@scribd.com>
2022-04-03 14:45:19 -0700

    more challenges

--- DigitSumChallenge/src/learnprogramming/Main.java
+++ DigitSumChallenge/src/learnprogramming/Main.java
@@ -3,13 +3,26 @@ package learnprogramming;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("The sum of the digits in 1980 is "+ sumDigits(1980));
-        System.out.println(isPalindrome(-1221));
-        sumFirstAndLastDigit(15);
-       int getEven = getEvenDigitSum(123456789);
-        System.out.println(getEven);
+        // write your code here
+//        System.out.println("The sum of the digits in 1980 is " + sumDigits(1980));
+//        System.out.println(isPalindrome(-1221));
+//        sumFirstAndLastDigit(15);
+//        int getEven = getEvenDigitSum(123456789);
+//        System.out.println(getEven);
+//        boolean shared = hasSharedDigit(15, 55);
+//        // System.out.println(shared);
+//        boolean sameLast = hasSameLastDigit(11, 22, 31);
+//        System.out.println(sameLast);
+//        int greatestDivisor = getGreatestCommonDivisor(25, 15);
+//        System.out.println(greatestDivisor);
+
+       //  FactorPrinter.printFactors(10);
+//         boolean perfect = PerfectNumber.isPerfectNumber(0);
+//        System.out.println(perfect);
+        NumberToWords.numberToWords(4000);
+
     }
+
     public static int sumDigits(int number) {
         if (number < 10) {
             return -1;
@@ -27,44 +40,46 @@ public class Main {
         return sum;
 
     }
-// Number Palindrome
-static boolean isPalindrome(int number) {
-    int startingNumber = number;
-    int reverse = 0;
-    if (number < 0) {
-        number = number * (-1);
-        startingNumber = startingNumber * (-1);
-    }
-    while (number > 0) {
-        //extract the last number
-        int lastDigit = number % 10;
-        // increase the place value and add to the last digit
-        reverse = reverse * 10 + lastDigit;
-        number /= 10;
 
-    }
-    return reverse == startingNumber;
-}
+    // Number Palindrome
+    static boolean isPalindrome(int number) {
+        int startingNumber = number;
+        int reverse = 0;
+        if (number < 0) {
+            number = number * (-1);
+            startingNumber = startingNumber * (-1);
+        }
+        while (number > 0) {
+            //extract the last number
+            int lastDigit = number % 10;
+            // increase the place value and add to the last digit
+            reverse = reverse * 10 + lastDigit;
+            number /= 10;
 
-// First and last Digit Sum
+        }
+        return reverse == startingNumber;
+    }
+
+    // First and last Digit Sum
     // log the first number =  number % 10
-public static int sumFirstAndLastDigit(int number) {
-    int firstDigit = 0;
-    int lastDigit = number % 10;
-    System.out.println("last digit: " + lastDigit);
-    if (number < -1) {
-        return -1;
+    public static int sumFirstAndLastDigit(int number) {
+        int firstDigit = 0;
+        int lastDigit = number % 10;
+        System.out.println("last digit: " + lastDigit);
+        if (number < -1) {
+            return -1;
+        }
+
+        while (number != 0) {
+            firstDigit = number % 10;
+            number /= 10;
+        }
+        System.out.println("First digit: " + firstDigit);
+        int sum = firstDigit + lastDigit;
+        System.out.println("The sum of these numbers: " + sum);
+        return sum;
     }
 
-    while (number != 0) {
-        firstDigit = number % 10;
-        number /= 10;
-    }
-    System.out.println("First digit: " + firstDigit);
-    int sum = firstDigit + lastDigit;
-    System.out.println("The sum of these numbers: " + sum);
-    return sum;
-}
     public static int getEvenDigitSum(int number) {
 
         int lastDigit;
@@ -83,6 +98,52 @@ public static int sumFirstAndLastDigit(int number) {
         System.out.println("Invalid input:");
         return -1;
     }
+// Shared digits
+    public static boolean hasSharedDigit(int num1, int num2) {
+        if ((num1 >= 10 && num1 <= 99) && (num2 >= 10 && num2 <= 99)) {
+            int firstDigit = num1 % 10;
+            int secondDigit = num2 % 10;
+            num1 /= 10;
+            num2 /= 10;
+            return num1 == num2 || num1 == secondDigit || firstDigit == num2;
+        } else
+            return false;
 
+    }
+// Checking if numbers have the same right most digit
+    public static boolean hasSameLastDigit(int firstDigit, int secondDigit, int thirdDigit) {
+        //numbers have to be valid between 10 and 1000
+        if ((firstDigit >= 10 && firstDigit <= 1000) && (secondDigit >= 10 && secondDigit <= 1000) && (thirdDigit >= 10 && thirdDigit <= 1000)) {
+            int firstOne = firstDigit % 10;
+            int secondOne = secondDigit % 10;
+            int thirdOne = thirdDigit % 10;
+            firstDigit /= 10;
+            secondDigit /= 10;
+            thirdDigit /= 10;
+            return firstDigit == secondOne || secondDigit == thirdOne || firstOne == thirdDigit || thirdDigit == secondDigit || firstOne == thirdOne;
+        } else return false;
+    }
+
+    public static boolean isValid(int number) {
+        if (number >= 10 && number <= 1000) {
+            return true;
+        } else return false;
+    }
+    //Common Divisor between two numbers The Greatest Common Denominators
+    //Euclid's algorithm   reduce the larger of the two numbers, repeating gives smaller pairs of numbers until the two numbers become equal.
+    public static int getGreatestCommonDivisor(int first, int second) {
+
+        if (first < 10 || second < 10) {
+            return -1;
+        }
+        while (first != second) {
+            if (first > second)
+                first = first - second;
+            else {
+                second = second - first;
+            }
+        }
+        return second;
+    }
 
 }
---
+++ DigitSumChallenge/src/learnprogramming/PerfectNumber.java
@@ -0,0 +1,19 @@
+package learnprogramming;
+
+public class PerfectNumber {
+    public static boolean isPerfectNumber(int number) {
+        if (number < 1) {
+            return false;
+        }
+        int perfectNum = 0;
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                perfectNum += i;
+            }
+        }
+        if (perfectNum == number) {
+            return true;
+        } return false;
+
+    }
+}
